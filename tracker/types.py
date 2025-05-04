efa877e8349484d9952dc3e0c1067b36cf323b44
Satyam Seth <satyam1998.1998@gmail.com>
2025-05-05 02:22:42 +0530

    Refactor for scrape page code

--- tracker/types.py
+++ tracker/types.py
@@ -1,6 +1,13 @@
 from typing import TypedDict, Optional
 
 
+class ScrapePageData(TypedDict):
+    title_selector: str
+    price_selector: str
+    rating_selector: str
+    url: str
+    source: str
+
 class ProductData(TypedDict):
     title: Optional[str]
     price: Optional[str]
