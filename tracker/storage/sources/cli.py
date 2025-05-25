edaa7b70f217706296bfb19b29725824505de960
Satyam Seth <satyam1998.1998@gmail.com>
2025-05-25 14:39:13 +0530

    Add sources cli command for get source by domain

--- tracker/storage/sources/cli.py
+++ tracker/storage/sources/cli.py
@@ -3,6 +3,7 @@ import argparse
 from .services import (
     create_source,
     get_source_by_id,
+    get_source_by_domain,
     list_sources,
     update_source,
     delete_source,
@@ -10,6 +11,7 @@ from .services import (
 
 
 def add_sources_subparsers(subparsers: argparse._SubParsersAction):
+    # Create
     create_parser = subparsers.add_parser("create", help="Create a new source")
     create_parser.add_argument("--name", required=True)
     create_parser.add_argument("--domain", required=True)
@@ -17,11 +19,18 @@ def add_sources_subparsers(subparsers: argparse._SubParsersAction):
     create_parser.add_argument("--price_selector", required=True)
     create_parser.add_argument("--rating_selector", required=True)
 
+    # Get by ID
     get_parser = subparsers.add_parser("get", help="Retrieve a source by ID")
     get_parser.add_argument("id", type=int)
 
+    # Get by Domain
+    get_domain = subparsers.add_parser("get-by-domain", help="Get source by Domain")
+    get_domain.add_argument("--domain", required=True)
+
+    # List
     subparsers.add_parser("list", help="List all sources")
 
+    # Update
     update_parser = subparsers.add_parser("update", help="Update a source by ID")
     update_parser.add_argument("id", type=int)
     update_parser.add_argument("--name")
@@ -30,6 +39,7 @@ def add_sources_subparsers(subparsers: argparse._SubParsersAction):
     update_parser.add_argument("--price_selector")
     update_parser.add_argument("--rating_selector")
 
+    # Delete
     delete_parser = subparsers.add_parser("delete", help="Delete a source by ID")
     delete_parser.add_argument("id", type=int)
 
@@ -49,10 +59,14 @@ async def handle_sources_commands(args: argparse.Namespace):
         source = await get_source_by_id(args.id)
         print("Retrieved:", source)
 
+    elif args.command == "get-by-domain":
+        product = await get_source_by_domain(args.domain)
+        print("Retrieved:", product)
+
     elif args.command == "list":
         sources = await list_sources()
-        for s in sources:
-            print(s)
+        for source in sources:
+            print(source)
 
     elif args.command == "update":
         source = await update_source(
