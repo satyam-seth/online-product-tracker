d113e4240d02e0c3c3ad1f43129353df2d2b6505
Satyam Seth <satyam1998.1998@gmail.com>
2025-06-04 22:32:58 +0530

    Add sources delete api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response, status
 
 from app.sources.schemas import SourceUpdate
 
 from .schemas import HealthCheck
 from .sources import SourceCreate, SourceOut
-from .sources import create_source, get_source_by_id, list_sources, update_source
+from .sources import (
+    create_source,
+    get_source_by_id,
+    list_sources,
+    update_source,
+    delete_source,
+)
 
 app = FastAPI()
 
@@ -47,3 +53,13 @@ async def source_update(source_id: int, updated_source: SourceUpdate):
         raise HTTPException(status_code=404, detail="Source not found")
 
     return source
+
+
+@app.delete("/sources/{source_id}", response_model=SourceOut, tags=["Sources"])
+async def source_delete(source_id: int):
+    succeed = await delete_source(source_id)
+
+    if not succeed:
+        raise HTTPException(status_code=404, detail="Source not found")
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
